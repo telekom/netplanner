cbeb67e5cada898b2104139350fa91b7f6f0c301
Marcel Fest <marcel.fest@telekom.de>
2021-08-27 14:17:24 +0200

    Moved some stuff a layer up

--- generate-proto.sh
+++ generate-proto.sh
@@ -4,5 +4,5 @@ export TEMP_DIR=$(mktemp -d)
 export FILENAME="frr-northbound.proto"
 export TEMP_FILE="${TEMP_DIR}/${FILENAME}"
 wget -O "${TEMP_FILE}" https://raw.githubusercontent.com/FRRouting/frr/master/grpc/${FILENAME}
-python -m grpc_tools.protoc  --python_out="${MIMIR_PATH}/providers/frr/client" --grpc_python_out="${MIMIR_PATH}/providers/frr/client" -I ${TEMP_DIR} ${FILENAME}
+python -m grpc_tools.protoc  --python_out="${MIMIR_PATH}/frr/client" --grpc_python_out="${MIMIR_PATH}/frr/client" -I ${TEMP_DIR} ${FILENAME}
 rm -r ${TEMP_DIR}
