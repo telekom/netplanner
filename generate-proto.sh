4d6524f28ec2bf850d86816703b8ebd996453b47
Marcel Fest <marcel.fest@telekom.de>
2021-08-30 18:38:33 +0200

    Added frr client generation with automatic code formatting

--- generate-proto.sh
+++ generate-proto.sh
@@ -5,4 +5,5 @@ export FILENAME="frr-northbound.proto"
 export TEMP_FILE="${TEMP_DIR}/${FILENAME}"
 wget -O "${TEMP_FILE}" https://raw.githubusercontent.com/FRRouting/frr/master/grpc/${FILENAME}
 python -m grpc_tools.protoc  --python_out="${MIMIR_PATH}/frr/client" --grpc_python_out="${MIMIR_PATH}/frr/client" -I ${TEMP_DIR} ${FILENAME}
+python -m black "${MIMIR_PATH}/frr/client"
 rm -r ${TEMP_DIR}
