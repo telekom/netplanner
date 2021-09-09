e2623dc1496f69aabcb053ddf858435bf9ca0666
Marcel Fest <marcel.fest@telekom.de>
2021-09-09 19:09:54 +0200

    removed the sriov provider from the templates

--- mimir/providers/sriov/tcam/__init__.py
+++
@@ -1,4 +0,0 @@
-# ref: https://mails.dpdk.org/archives/dev/2018-October/115898.html
-# ref: https://marcelwiget.blog/2021/02/15/vxlan-offload-using-tc-flower-on-connectx-5/
-# ref: https://community.mellanox.com/s/article/Configuring-VXLAN-Encap-Decap-Offload-Using-tc
-# ref: https://netdevconf.info/0x15/session.html?Where-turbo-boosting-TC-flower-control-path-had-led-us-to
