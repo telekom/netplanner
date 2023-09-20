96ee5b2dc2279d2679b0e208beae23424938362d
christopher.dziomba@telekom.de <christopher.dziomba@telekom.de>
2023-09-20 10:58:11 +0200

    Fix eswitch mode

--- netplanner/sriov/__main__.py
+++ netplanner/sriov/__main__.py
@@ -64,4 +64,4 @@ def main(configuration: NetplannerConfig):
             )
             device.set_sriov_numvfs(interface_config.virtual_function_count)
             if interface_config.embedded_switch_mode is not None:
-                device.set_eswitch_mode(str(interface_config.embedded_switch_mode))
+                device.set_eswitch_mode(interface_config.embedded_switch_mode.value)
